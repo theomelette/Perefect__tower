52e664e627172b74ec2db2dbc8a8ddc8ea5eff50
Amit <amitnaipaul@gmail.com>
2021-03-07 10:01:44 -0500

    added show and index for gpu and tower

--- app/controllers/graphics_cards_controller.rb
+++ app/controllers/graphics_cards_controller.rb
@@ -1,2 +1,10 @@
 class GraphicsCardsController < ApplicationController
+
+    def index
+        @graphics_cards = GraphicsCard.all
+    end
+
+    def show
+        @graphics_card = GraphicsCard.find(params[:id])
+    end
 end
--- app/controllers/towers_controller.rb
+++ app/controllers/towers_controller.rb
@@ -1,2 +1,11 @@
 class TowersController < ApplicationController
+
+    def index
+        @towers = Tower.all
+    end
+
+    def show
+        @tower = Tower.find(params[:id])
+    end
+
 end
