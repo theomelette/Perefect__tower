1d2b8b9c9e5e1fbed5c9976544b19f4e4d69b4d9
Amit <amitnaipaul@gmail.com>
2021-03-08 22:15:24 -0500

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :pcs
-  resources :cpus
-  resources :rams
-  resources :graphics_cards
-  resources :towers
+  resources :cpus, only: [:index, :show]
+  resources :rams, only: [:index, :show]
+  resources :graphics_cards, only: [:index, :show]
+  resources :towers, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
